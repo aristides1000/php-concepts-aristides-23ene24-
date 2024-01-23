d8d5d94866c04a7c251ff86eb6e9da345511562b
aristides1000 <aristides1000@gmail.com>
2024-01-23 10:26:16 -0400

    Feat: finish exercise 1 of sintax

---
+++ 01-sintaxis/ejercicio_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ejercicio 1 | sintaxis</title>
+</head>
+<body>
+  <h1>Mi primera pagina PHP</h1>
+
+  <?php
+    echo "Hola mundo";
+  ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index | PHP</title>
+</head>
+<body>
+  <ol>
+    <li>sintaxis</li>
+    <ul>
+      <li><a href="01-sintaxis/ejercicio_1.php">ejericio 1</a></li>
+      <li><a href="01-sintaxis/ejercicio_2.php">ejericio 2</a></li>
+      <li><a href="01-sintaxis/ejercicio_3.php">ejericio 3</a></li>
+    </ul>
+  </ol>
+</body>
+</html>
